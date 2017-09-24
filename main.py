4792bb05df33b4f42c6099d07f4b786ac3a8b34b
Minh Nguyen <minh.nguyen98@gmail.com>
2017-09-24 10:23:31 -0400

    main takes in articlequota, companies by argument

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from parse import *
 from events import *
 from sms import *
@@ -6,8 +7,11 @@ from sms import *
 def main():
 	er_client = getERClient()
 	language_client = getLanguageClient()
-	companies = ['apple', 'microsoft','google','amazon',
-        'facebook','samsung', 'verizon','at&t']
+	# companies = ['apple', 'microsoft','google','amazon',
+    #     'facebook','samsung', 'verizon','at&t']
+	
+	articleQuota = sys.argv[1]
+	companies = sys.argv[2:]
 
 	data_path = 'data.pickle'
 	data = getData(data_path)
@@ -18,28 +22,17 @@ def main():
 	number_from = "+14804000465"
 	number_to = "+14806944346"
 
-	articleQuota = 10
 
 	for company in companies:
-		keywords = data[company]
-<<<<<<< HEAD
-		for keyword, salience in keywords:
-			pairing = [company, keyword]
-			articles = getArticles(er_client, pairing, 1)
-			if articles:
-				for article in articles:
-					message_body = ''
-					message_body += '------------------------Article------------------------' + '\n'
-=======
+		keywords, salience  = zip(*data[company])
 		articles = getArticles(er_client, company, keywords, articleQuota)
-		message_Body = "----------" + company + "----------"
+		message_body = "----------" + company + "----------"
 		message = sendMessage(twilio_client, number_from, number_to, message_body)
 
 		if articles:
 			for article in articles:
 					message_body = ""
 					message_body += '----------Article----------' + '\n'
->>>>>>> 9bcebb3f300c675d4cc68947c2266aa7ecfc69af
 					message_body += "Title: " + article["title"] + '\n'
 					message_body += "Date: " + article["date"] + '\n'
 					message_body += "Time: " + article["time"] + '\n'
