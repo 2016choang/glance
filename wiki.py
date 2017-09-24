1c5edfce3639de6b3b166e4e996a494de8dcd99e
Kevin Zheng <kevzheng@umich.edu>
2017-09-24 01:29:48 -0400

    html parsing

--- wiki.py
+++ wiki.py
@@ -2,28 +2,25 @@ from eventregistry import *
 import sys
 import requests
 import json
+from lxml import html
 
-def wiki(client, company):
-    uri = client.getConceptUri(company)
+def getWiki(client, company):
+    #uri = client.getConceptUri(company)
+    concepts = client.suggestConcepts(company, ["org"])
+    if concepts:
+        wiki_param = concepts[0]["uri"].split("/")[-1]
 
-
-    print(uri)
-
-    wiki_param = uri.split('/')[-1]
-
-    print(wiki_param)
-
-
-    request ='''https://en.wikipedia.org/w/api.php?action=query&titles={}&prop=revisions&rvprop=content&format=json'''.format(wiki_param)
+    request ='''https://en.wikipedia.org/w/api.php?action=parse&page={}&prop=text&format=json'''.format(wiki_param)
 
     r = requests.get(request)
+    body_html = r.json()["parse"]["text"]["*"]
+    body = html.document_fromstring(body_html).text_content()
 
-    with open('data.json', 'w') as fp:
-        json.dump(r.json(), fp, indent=4)
+    return body
 
 def main():
     er = EventRegistry(apiKey="4c927d75-f35a-4646-910a-9f071768c8b1")
-    wiki(er, "apple")
+    getWiki(er, "apple")
 
 if __name__ == "__main__":
     main()
